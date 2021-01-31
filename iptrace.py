11782b23d1801ac52f07a5ffb7f448163af6c0ad
anonymous <78273014+noobforanonymous@users.noreply.github.com>
2021-01-31 09:49:03 +0530

    Update iptrace.py

--- iptrace.py
+++ iptrace.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument ("-v", help= "target/host IP address", type=str, dest='target', required=True )
 args = parser.parse_args()
 
-red = '\033[31m'
+green = '\033[31m'
 yellow = '\033[93m'
 lgreen = '\033[92m'
 clear = '\033[0m'
